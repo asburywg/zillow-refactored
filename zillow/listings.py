1fa049509a6047d3d6e7cac60223973dc603944f
Will Asbury <asburywg@gmail.com>
2022-10-28 14:38:25 -0400

    cache listings

--- zillow/listings.py
+++ zillow/listings.py
@@ -3,12 +3,11 @@ import logging
 import re
 from typing import List
 
+from zillow.cache_util import cache_intermediary
 from zillow.session import Session
 from zillow.zipcode_util import fetch_zipcodes
 
-
 log = logging.getLogger(__name__)
-
 
 """
 >>> Search("columbus", "ohio")
@@ -19,28 +18,34 @@ log = logging.getLogger(__name__)
 
 class Search:
     BASE_URL = "https://www.zillow.com"
+    DEFAULT_DATA_DIR = "./data"
 
     def __init__(self, city: str = None, state: str = None,
                  exclude_zipcodes: List[int] = None,
-                 zipcodes: List[int] = None):
+                 zipcodes: List[int] = None, data_dir: str = DEFAULT_DATA_DIR):
         """
         Search listings by city/state (using zipcode lookup) or a list of zipcodes
         :param city: location used for zipcode lookup
         :param state: location used for zipcode lookup
         :param exclude_zipcodes: list of zipcodes to exclude from city/state zipcode lookup
         :param zipcodes: skip zipcode lookup and only search for specified list
+        :param data_dir: root directory to store all output
         """
         # user input should be either city/state or [zipcodes]
         if not (city and state) and not zipcodes or (city and state and zipcodes):
             raise TypeError("Specify *either* city and state or a list of zipcodes to search.")
         self.zipcodes = zipcodes if zipcodes else fetch_zipcodes(city, state, exclude_zipcodes)
         log.info(f"Searching {len(self.zipcodes)} zipcodes: {self.zipcodes}")
+        self.data_dir = data_dir
         self.session = Session()
 
-    def get_all_listings(self):
+    def get_all_listings(self, cache: bool = False, cache_dir: str = "cache"):
         for zc in self.zipcodes:
             log.info(f'Scraping listings in {zc}')
-            return self._zipcode_listings(zc)
+            listings = self._zipcode_listings(zc)
+            if cache:
+                cache_intermediary(zc, listings, f"{self.data_dir}/{cache_dir}")
+            return listings
 
     def _zipcode_listings(self, zipcode):
         for_rent = self._scrape_results(f"{self.BASE_URL}/homes/for_rent/{zipcode}_rb/")
@@ -58,9 +63,11 @@ class Search:
             raise Exception("Bad result from Zillow", page_text)
         data = json.loads(query_state.group(1))
         """control pagination"""
-        current_page = data.get("queryState").get("pagination").get("currentPage") if "pagination" in data.get("queryState") else 1
+        current_page = data.get("queryState").get("pagination").get("currentPage") if "pagination" in data.get(
+            "queryState") else 1
         total_pages = data.get('cat1').get("searchList").get("totalPages")
-        next_url = data.get('cat1').get("searchList").get("pagination").get('nextUrl') if "pagination" in data.get('cat1').get("searchList") else None
+        next_url = data.get('cat1').get("searchList").get("pagination").get('nextUrl') if "pagination" in data.get(
+            'cat1').get("searchList") else None
         total_listings = data.get("cat1").get("searchList").get("totalResultCount")
         """results"""
         res = data.get('cat1').get("searchResults").get("listResults")
