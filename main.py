8f429a8589db59e9ab926fc616737bda7f73d6b0
Will Asbury <asburywg@gmail.com>
2022-10-28 12:47:40 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+
+
+"""
+Input: city, state ?? list of zip codes to exclude || list of zip codes
+use user input zip codes or fetch zip codes for city, state (less optional exclude list)
+for each zip code, search zillow for FOR_SALE and FOR_RENT listings (in parallel w/ diff UA?)
+concat and cache raw listing data from zillow (JSON)
+format and output data for export (CSV)
+"""
+
