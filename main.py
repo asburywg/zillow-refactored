0a56b429c2140f51d8ec7763222ab3153ed76b0e
Will Asbury <asburywg@gmail.com>
2022-11-02 20:56:12 -0400

    output file

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import logging
+from datetime import datetime
 
-from zillow.file_util import export_csv
+from zillow.file_util import export_csv, write_json
 from zillow.listings import Search
 from zillow.formatter import *
 from zillow.property import Apartments
 
 logging.basicConfig(level=logging.INFO)
 
+APARTMENT_URL_FILE = "./data/results/{}/{}-apartments.json"
+LISTINGS_FILE = "./data/results/{}/{}-listings.csv"
 
-def format_data(listings, include_apartments=False):
+
+def format_data(listings, apartment_file=None, include_apartments=False):
     """transform and format listing data"""
 
     data = ListingFormatter(listings)
@@ -23,6 +27,8 @@ def format_data(listings, include_apartments=False):
 
     """apartments"""
     apartment_urls = data.apartment_urls()
+    if apartment_file:
+        write_json(apartment_urls, apartment_file)
 
     data.remove_apartments()
 
@@ -45,10 +51,11 @@ def format_data(listings, include_apartments=False):
 def main():
     city, state = "columbus", "ohio"
     search = Search(city=city, state=state)
-    # TODO: collect stats by zipcode (rent/sale)
     listings = search.get_all_listings(read_cache=True)
-    df = format_data(listings)
-    export_csv(df, "./data/results/col-listings-no-apt.csv")
+    # format and export
+    date = datetime.now().date().strftime("%Y%m%d")
+    df = format_data(listings, APARTMENT_URL_FILE.format(date, city))
+    export_csv(df, LISTINGS_FILE.format(date, city))
 
 
 if __name__ == "__main__":
